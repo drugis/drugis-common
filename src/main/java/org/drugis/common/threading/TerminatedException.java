38bb0b3f4c07aa6e8e619a9c7664ded5f79e4940
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-09-16 11:53:08 +0200

    Add thread wrapper, and version to 0.1

--- src/main/java/org/drugis/common/threading/TerminatedException.java
+++ src/main/java/org/drugis/common/threading/TerminatedException.java
@@ -4,6 +4,8 @@ package org.drugis.common.threading;
  * Exception thrown when Suspendable is terminated.
  */
 public class TerminatedException extends Exception {
+	private static final long serialVersionUID = 6661473521989247040L;
+
 	public TerminatedException() {
 		super("Runnable terminated.");
 	}
