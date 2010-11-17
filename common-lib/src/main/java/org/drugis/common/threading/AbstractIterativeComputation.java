0b468c832cd43e5fb627d6f077ff36c08edc1b7c
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-11-17 14:14:16 +0100

    Implement AbstractIterativeComputation

---
+++ common-lib/src/main/java/org/drugis/common/threading/AbstractIterativeComputation.java
@@ -0,0 +1,32 @@
+package org.drugis.common.threading;
+
+public abstract class AbstractIterativeComputation implements IterativeComputation {
+	private final int d_totalIterations;
+	private int d_iteration;
+
+	public AbstractIterativeComputation(int totalIterations) {
+		d_totalIterations = totalIterations;
+		d_iteration = 0;
+	}
+
+	public void finish() {
+	}
+
+	public int getIteration() {
+		return d_iteration;
+	}
+
+	public int getTotalIterations() {
+		return d_totalIterations;
+	}
+
+	public void initialize() {
+	}
+
+	public void step() {
+		doStep();
+		++d_iteration;
+	}
+	
+	public abstract void doStep();
+}
