c920f223db6bab4929645194bab630ff09f09db9
Daan Reid <osmosisch@gmail.com>
2011-10-19 15:41:22 +0200

    Make ObserverManager thread safe (fixed #376)

--- common-lib/src/main/java/org/drugis/common/beans/ObserverManager.java
+++ common-lib/src/main/java/org/drugis/common/beans/ObserverManager.java
@@ -11,6 +11,7 @@ import com.jgoodies.binding.beans.Observable;
  * Class to manage PropertyChangeListener to facilitate implementation of Observable.
  */
 public class ObserverManager {
+	private static final PropertyChangeListener[] LISTENER_ARRAY = new PropertyChangeListener[]{};
 	private Observable d_source;
 	private Collection<PropertyChangeListener> d_listeners = createCollection();
 	
@@ -23,16 +24,28 @@ public class ObserverManager {
 	}
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		d_listeners.add(listener);
+		synchronized (d_listeners) {
+			d_listeners.add(listener);			
+		}
 	}
 	
 	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		d_listeners.remove(listener);
+		synchronized (d_listeners) {
+			d_listeners.remove(listener);			
+		}
 	}
 	
 	public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
 		PropertyChangeEvent event = new PropertyChangeEvent(d_source, propertyName, oldValue, newValue);
-		for (PropertyChangeListener l : d_listeners) {
+		
+		// Copy the list of listeners for thread safety
+		// We don't synchronize the for-loop because listeners might be doing time-consuming stuff
+		PropertyChangeListener[] arr;
+		synchronized (d_listeners) {
+			arr = d_listeners.toArray(LISTENER_ARRAY);			
+		}
+		
+		for (PropertyChangeListener l : arr) {
 			l.propertyChange(event);
 		}
 	}
--- common-lib/src/test/java/org/drugis/common/ObserverManagerTest.java
+++ common-lib/src/test/java/org/drugis/common/ObserverManagerTest.java
@@ -1,21 +1,36 @@
 package org.drugis.common;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import org.drugis.common.beans.ObserverManager;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jgoodies.binding.beans.Observable;
 
 public class ObserverManagerTest {
+	
+	private ObserverManager d_om;
+	private Observable d_source;
+	private boolean d_running;
+	protected Throwable d_thrown;
+
+	@Before
+	public void setUp() {
+		d_source = createMock(Observable.class);
+		d_om = new ObserverManager(d_source);
+		d_running = true;
+	}
+	
 	@Test
 	public void testSingleListener() {
-		Observable source = createMock(Observable.class);
-		ObserverManager d_om = new ObserverManager(source);
 		
-		PropertyChangeListener listener = JUnitUtil.mockStrictListener(source, "test", "X", "Y");
+		PropertyChangeListener listener = JUnitUtil.mockStrictListener(d_source, "test", "X", "Y");
 		d_om.addPropertyChangeListener(listener);
 		d_om.firePropertyChange("test", "X", "Y");
 		verify(listener);
@@ -23,12 +38,9 @@ public class ObserverManagerTest {
 	
 	@Test
 	public void testMultipleListener() {
-		Observable source = createMock(Observable.class);
-		ObserverManager d_om = new ObserverManager(source);
-		
-		PropertyChangeListener listener0 = JUnitUtil.mockStrictListener(source, "test", "X", "Y");
+		PropertyChangeListener listener0 = JUnitUtil.mockStrictListener(d_source, "test", "X", "Y");
 		d_om.addPropertyChangeListener(listener0);
-		PropertyChangeListener listener1 = JUnitUtil.mockStrictListener(source, "test", "X", "Y");
+		PropertyChangeListener listener1 = JUnitUtil.mockStrictListener(d_source, "test", "X", "Y");
 		d_om.addPropertyChangeListener(listener1);
 		d_om.firePropertyChange("test", "X", "Y");
 		verify(listener0);
@@ -37,9 +49,6 @@ public class ObserverManagerTest {
 	
 	@Test
 	public void testRemoveListener() {
-		Observable source = createMock(Observable.class);
-		ObserverManager d_om = new ObserverManager(source);
-		
 		PropertyChangeListener listener = createMock(PropertyChangeListener.class);
 		replay(listener);
 		
@@ -47,5 +56,61 @@ public class ObserverManagerTest {
 		d_om.removePropertyChangeListener(listener);
 		d_om.firePropertyChange("test", "X", "Y");
 		verify(listener);
+	}
+	
+	/**
+	 * @see http://mantis.drugis.org/view.php?id=376
+	 */
+	@Test
+	public void testThreadSafety() throws Throwable {
+		ThreadGroup group = new ThreadGroup("happy") {
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				d_thrown = e;
+			}
+		};
+		
+		// Thread that adds listeners (which run for a while to trigger race condition)
+		Thread listen = new Thread(group, new Runnable() {
+			public void run() {
+				while(d_running) {
+					d_om.addPropertyChangeListener(new PropertyChangeListener() {
+						public void propertyChange(PropertyChangeEvent evt) {
+							int untilImDead = 30;
+							try {
+								Thread.sleep(untilImDead);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			}
+		});
+		
+		// Thread that fires events
+		Thread fire = new Thread(group, new Runnable() {
+			public void run() {
+				while(d_running) {
+					d_om.firePropertyChange("je moeder", "je vader", "je stief");
+				}
+			}
+		});
+		
+		listen.start();
+		fire.start();
+		
+		// Wait for things to go wrong
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		d_running = false;
+		
+		// Rethrow exception (if any)
+		if (d_thrown != null) {
+			throw d_thrown;
+		}
 	}
 }
