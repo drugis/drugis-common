63b05c2d3fd26082948691043b61d9b62e8a0b61
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2012-08-10 12:39:16 +0200

    Move Table components from ADDIS (story #545)

---
+++ common-gui/src/main/java/org/drugis/common/gui/table/RenderAsImage.java
@@ -0,0 +1,9 @@
+package org.drugis.common.gui.table;
+
+/**
+ * Interface that signals to {@link TableCopyHandler} that the CellRenderer
+ * should be treated as an image rather than text.
+ */
+public interface RenderAsImage {
+
+}
